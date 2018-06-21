88ad7be602778261e2499939909cfc882aa96bd9
ograni <ograni@hanmail.net>
2018-06-21 21:24:41 +0900

    6월 21일 2교시 백업
    2교시까지 백업

--- 01_jQuery_basic/ex08_imgslider/js/style2.js
+++ 01_jQuery_basic/ex08_imgslider/js/style2.js
@@ -1,14 +1,24 @@
 $(function(){
     var interval = 1000;
     $('.slideshow').each(function(){
+        var timer;
         var container = $(this);
         function switchImg(){
-            var imgs = container.find('img');
-            var first = imgs.eq(0);
-            var second = imgs.eq(1);
+            var anchors = container.find('a');
+            var first = anchors.eq(0);
+            var second = anchors.eq(1);
             first.fadeOut().appendTo(container);
             second.fadeIn();
         }
-        setInterval(switchImg, interval);
+        function startTimer(){
+            timer = setInterval(switchImg, interval);
+        }
+        function stopTimer(){
+            clearInterval(timer);
+        }
+        container.find('a').hover(stopTimer, startTimer);
+        //hover(올렸을때, 올리지않았을때)
+        startTimer(); //일단 시작하고
+
     });
 });
---
+++ 선생님이치신거/ex08_imgslider/js/script2.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function(){
+	var interval = 3000;
+	$('.slideshow').each(function(){
+		var container = $(this);
+		function switchImg(){
+			var imgs = container.find('img');
+			var first = imgs.eq(0);
+			var second = imgs.eq(1);
+			first.fadeOut().appendTo(container);
+			second.fadeIn();
+		}
+		setInterval(switchImg,interval);
+	});
+});
