ecfae31c503868a279d45d2514438dc97cab7eea
ograni <ograni@hanmail.net>
2018-06-28 20:21:41 +0900

    6월 28일 1교시 백업
    1교시 수업 내용 백업

---
+++ 02_jQuery_middle/ex06/js/style1.js
@@ -0,0 +1,10 @@
+$(function(){
+    var el = $('#nav > ul > li');
+    var urls = [
+        //배열선언
+        'http://www.hschool.co.kr',
+        'http://www.daum.net',
+        'http://www.nate.com',
+        'http://www.yahoo.com'
+    ]
+});
