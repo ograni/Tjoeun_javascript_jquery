ecbc06d24540b821edb7bbf0bd8721aa468549f5
한솔집pc <ograni@hanmail.net>
2018-07-02 00:26:33 +0900

    숙제 및 복습 : 20180702

--- 02_jQuery_middle/ex07/ex_menu_width1/js/menu_width.js
+++ 02_jQuery_middle/ex07/ex_menu_width1/js/menu_width.js
@@ -1,15 +1,25 @@
 ﻿jQuery(document).ready(function(){
-	
+	var menu = $('#animate > li');
+	menu.hover(
+		function(){
+			var targetMenu = $(this);
+			var menuImage = targetMenu.find('.menu_image');
+			var imagesWidth = menuImage.find('img').innerWidth();
+			var imgOn = targetMenu.find('div.on');
+			var imgOff = targetMenu.find('div.off');
+			imgOn.css('display','block');
+			imgOff.css('display','none');
+			menuImage.animate({width:imagesWidth},{duration:350,queue:false})
+		},
+		function(){
+			var targetMenu = $(this);
+			var menuImage = targetMenu.find('.menu_image');
+			var imagesWidth = menuImage.find('img').innerWidth();
+			var imgOn = targetMenu.find('div.on');
+			var imgOff = targetMenu.find('div.off');
+			imgOn.css('display','none');
+			imgOff.css('display','block');
+			menuImage.animate({width:0},{duration:350,queue:false})
+		}
+	);
 });
-
-
-
-
-
-
-
-
-
-
-
-
