983f3d31fcdd67d0cea3520febfa94de12d7973f
한솔 아주 pc <ograni@hanmail.net>
2018-07-02 13:07:42 +0900

    학원복습 : 20180702

--- 02_jQuery_middle/ex05/js/style1.js
+++ 02_jQuery_middle/ex05/js/style1.js
@@ -1,4 +1,4 @@
-$(function(){
+/*$(function(){
     $('#nav > ul > #springBtn').bind('click',function(){
         $('#image > img').attr({src:'images/spring.jpg', alt:'봄'})
     });
@@ -11,4 +11,20 @@ $(function(){
     $('#nav > ul > #winterBtn').bind('click',function(){
         $('#image > img').attr({src:'images/winter.jpg', alt:'겨울'})
     });
+});*/
+
+$(function(){
+    $('#springBtn').click(function(){
+        $('#image img').attr({src:'images/spring.jpg', art:'봄'});
+    });
+    $('#summerBtn').click(function(){
+        $('#image img').attr({src:'images/summer.jpg', art:'여름'});
+    });
+    $('#fallBtn').click(function(){
+        $('#image img').attr({src:'images/fall.jpg', art:'가을'});
+    });
+    $('#winterBtn').click(function(){
+        $('#image img').attr({src:'images/winter.jpg', art:'겨울'});
+    });
+
 });
--- 02_jQuery_middle/ex06/js/style1.js
+++ 02_jQuery_middle/ex06/js/style1.js
@@ -1,3 +1,53 @@
+/*$(function(){
+    var el = $('#nav > ul > li');
+    var urls = [
+        //배열선언
+        'http://www.hschool.co.kr',
+        'http://www.daum.net',
+        'http://www.nate.com',
+        'http://www.yahoo.com'
+    ]
+    el.on({
+        mouseover:function(){
+            var tg = $(this);
+            tg.addClass('on');
+        },
+        mouseout:function(){
+            var tg = $(this);
+            tg.removeClass('on');
+        },
+        click:function(event){
+            var tg = $(this);
+            event.preventDefault();
+            var i = tg.index();
+            var linkURL = urls[i];
+            // location.href = linkURL;
+            window.open(linkURL);
+        }
+    });
+});*/
+
+
+// $(function(){
+//     var menu = $('#nav li');
+//     var urls = [
+//         'http://www.hschool.co.kr',
+//         'http://www.daum.net',
+//         'http://www.nate.com',
+//         'http://www.yahoo.com'
+//     ]
+//     menu.on({
+//         hover:function(){
+//                 // var hoverMenu = $(this);
+//                 // this.addClass('on');
+//                 console.log('dd');
+//             },
+//             function(){
+//
+//             }
+//         }
+//     );
+// });
 $(function(){
     var el = $('#nav > ul > li');
     var urls = [
@@ -21,8 +71,8 @@ $(function(){
             event.preventDefault();
             var i = tg.index();
             var linkURL = urls[i];
-            location.href = linkURL;
-            //window.open(linkURL);
+            // location.href = linkURL;
+            window.open(linkURL);
         }
     });
 });
