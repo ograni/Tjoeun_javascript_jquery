e021fce6b410c1f617825a4b39596b0ba490515e
한솔 아주 pc <ograni@hanmail.net>
2018-07-03 16:54:26 +0900

    복습 : 20180703

--- 02_jQuery_middle/ex07/js/banner.js
+++ 02_jQuery_middle/ex07/js/banner.js
@@ -1,11 +1,12 @@
 $(function(){
-    var current = 0;
+    var current = 0; //현재 위치의 변수를 담고
     var banner = $('#wrapper li');
     setInterval(function(){
         var prev = banner.eq(current);
         move(prev, 0, '-100%'); /* 0 -> 100% 으로 이동*/ //move는 제이쿼리 메소드가 아니라 사용자가 만드는 함수임
         current++;
         if(current == banner.size()) current=0; //size는 몇개냐 html에서 늘어나도 수정하지 않아도 됨.
+        // if(current == banner.size()){current=0;}
         var next = banner.eq(current);
         move(next,'100%',0); /*100% -> 0 으로 이동*/
     },1000);
