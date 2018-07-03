e42152e5c06b4e24dbfec1770d7054f724ade24e
더조은학원PC <ograni@hanmail.net>
2018-07-03 22:23:21 +0900

    7월 3일 2교시 3교시 백업

--- 02_jQuery_middle/ex09/js/gnb.js
+++ 02_jQuery_middle/ex09/js/gnb.js
@@ -10,7 +10,7 @@
     menu.on({
         mouseover:function(){
             var tg = $(this);
-            menu.removeClass('on');
+            tg.removeClass('on');
             tg.addClass('on');
             var th = menuHeight + tg.find('>.sGnbArea').height();
             //th = 1뎁스 높이 + 2뎁스 높이
@@ -18,9 +18,47 @@
         },
         mouseout:function(){
             var tg = $(this);
-            menu.removeClass('on');
+            tg.removeClass('on');
             //th = 1뎁스 높이 + 2뎁스 높이
             wrap.stop().animate({height:menuHeight});
+            onActive();
         }
     });
+
+    menu.each(function(i){
+        var tg = $(this); //원뎁스 li
+        var sub = tg.find('>sGnbArea > ul > li'); //투뎁스 서브메뉴 li
+        var menuURL = tg.children('a').attr('href');
+        // menuURL = 1뎁스의 href 값
+        var active = pageURL.indexOf(menuURL);
+        //pageURL이 manuURL이랑 맞는지 string 순번으로 확인함
+        // 만약에 값이 없으면 -1 이 들어감
+        if(active > -1) activeMenu = tg;
+        //-1이상 즉 pageURL이랑 menuURL이 맞는거
+        sub.each(function(j){
+            var tg = $(this); //순환하는 투뎁스
+            var subURL = tg.children('a').attr('href');
+            ative = pageURL.indexOf(subURL);
+            if(active > -1 ) activeMenu = tg;
+        });
+        sub.on({
+            mouseover:function(){
+                var tg = $(this); //마우스가 올라간 투뎁스
+                sub.removeClass('on');
+                tg.addClass('on');
+            },
+            mouseout:function(){
+                var tg = $(this);
+                tg.removeClass('on');
+                onActive();
+            }
+        });
+    });
+    onActive();
+    function onActive(){
+        if(activeMenu) activeMenu.trigger('mouseover'); //mouseover를 하지 않아도 마우스오버를 한 효과
+    }
 });
+
+
+//이벤트 버블 칮이볼것
---
+++ 선생님이치신거/02_jQuery_middle/ex09/js/gnb.js
@@ -0,0 +1,78 @@
+﻿$(function(){
+    var menu = $('#gnb > li');
+	// 1뎁스 메뉴
+	var wrap = $('#gnbWrap');
+	// 1뎁스와 2뎁스를 감싸고 있는 div
+	var menuHeight = menu.children('a').height();
+	// 1뎁스의 높이
+	var pageURL = location.href;
+	// pageURL = http://www.hschool.co.kr/sub02/2depth_menu.html
+	var activeMenu;
+
+	menu.on({
+		mouseover:function(){
+			var tg = $(this);
+			menu.removeClass('on');
+			tg.addClass('on');
+			var th = menuHeight + tg.find('>.sGnbArea').height();
+			// th = 1뎁스 높이 + 2뎁스 높이
+			wrap.stop().animate({height:th});
+		},
+		mouseout:function(){
+			var tg = $(this);
+			tg.removeClass('on');
+
+			// th = 1뎁스 높이 + 2뎁스 높이
+			wrap.stop().animate({height:menuHeight});
+			onActive();
+		}
+	});
+	
+	menu.each(function(i){
+		var tg = $(this);
+		var sub = tg.find('>.sGnbArea > ul > li');
+		var menuURL = tg.children('a').attr('href');
+		// menuURL = 1뎁스의 href 값
+		var active = pageURL.indexOf(menuURL);
+		// http://www.hschool.co.kr/sub02/2depth_menu.html
+		// 3depth_menu.html  -1
+		if(active > -1) activeMenu = tg;
+		sub.each(function(j){
+			var tg = $(this);
+			var subURL = tg.children('a').attr('href');
+			active = pageURL.indexOf(subURL);
+			if(active>-1)activeMenu= tg;
+		});
+		sub.on({
+			mouseover:function(){
+				var tg = $(this);
+				sub.removeClass('on');
+				tg.addClass('on');
+			},
+			mouseout:function(){
+				var tg = $(this);
+				tg.removeClass('on');
+				//onActive();
+			}
+		});
+	});
+	
+	onActive();
+	
+	function onActive(){
+		if(activeMenu) activeMenu.trigger('mouseover');
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
