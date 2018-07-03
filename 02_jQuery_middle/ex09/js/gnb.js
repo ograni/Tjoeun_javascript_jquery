0a0ab74ceb2d103fb6ab6c9721f0cbda345e1cb3
더조은학원PC <ograni@hanmail.net>
2018-07-03 20:26:10 +0900

    7월 3일 1교시 백업

--- 02_jQuery_middle/ex09/js/gnb.js
+++ 02_jQuery_middle/ex09/js/gnb.js
@@ -1,3 +1,26 @@
 ﻿$(function(){
-    
+    var menu = $('#gnb > li'); //1뎁스
+    var wrap = $('#gnbWrap'); // 1뎁스와 2뎁스를 감싸고 있는놈
+    var menuHeight = menu.children('a').height();
+    //1뎁스의 자식 a 태그 높이
+    var pageURL = location.href;
+    //pageURL = http://...../sub/2depthPage.html
+    var activeMenu;
+
+    menu.on({
+        mouseover:function(){
+            var tg = $(this);
+            menu.removeClass('on');
+            tg.addClass('on');
+            var th = menuHeight + tg.find('>.sGnbArea').height();
+            //th = 1뎁스 높이 + 2뎁스 높이
+            wrap.stop().animate({height:th});
+        },
+        mouseout:function(){
+            var tg = $(this);
+            menu.removeClass('on');
+            //th = 1뎁스 높이 + 2뎁스 높이
+            wrap.stop().animate({height:menuHeight});
+        }
+    });
 });
