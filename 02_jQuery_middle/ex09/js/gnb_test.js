38fa02d72a342089e0a3f53add9ee0f8700e5181
한솔 아주 pc <ograni@hanmail.net>
2018-07-04 16:40:38 +0900

    복습 : 20180704

---
+++ 02_jQuery_middle/ex09/js/gnb_test.js
@@ -0,0 +1,41 @@
+﻿$(function(){
+    //상단메뉴를 변수로 잡아주고
+    var menu = $('#gnb > li');
+    var wrap = $('#gnbWrap');
+    var menuHeight = menu.children('a').height();
+    menu.on({
+        mouseover:function(){
+            var tg = $(this);
+            tg.removeClass('on');
+            tg.addClass('on');
+            var th = menu.height() + tg.find('>.sGnbArea').height();
+            wrap.stop().animate({height:th});
+        },
+        mouseout:function(){
+            var tg = $(this);
+            tg.removeClass('on');
+
+            wrap.stop().animate({height:menu.find('>a').height()});
+        }
+    });
+
+    menu.each(function(){
+        var tg = $(this);
+        var sub = tg.find('.sGnbArea > ul > li');
+        sub.on({
+            mouseover:function(){
+                var subtg = $(this);
+                tg.removeClass('on');
+                tg.addClass('on');
+            },
+            mouseout:function(){
+                var subtg = $(this);
+                tg.removeClass('on');
+            }
+        });
+    });
+
+});
+
+
+//이벤트 버블 칮이볼것
