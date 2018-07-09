18cf65c23c1076625c108f6c82c8727d92aaf1e1
더조은학원PC <ograni@hanmail.net>
2018-07-09 22:16:51 +0900

    7월 9일 2교시 3교시 백업

--- 03_jQuery_advance/ex03_slide_banner/js/slide_banner.js
+++ 03_jQuery_advance/ex03_slide_banner/js/slide_banner.js
@@ -1,3 +1,44 @@
-﻿$(function(){ 
-    
+﻿$(function(){
+    var visual = $('#brandVisual > ul >li');
+    var button = $('#buttonList > li');
+    var current = 0;
+    var setIntervalId;
+
+    function timer(){
+        setIntervalId = setInterval(function(){
+            var n = current + 1;
+            if(n == button.size()){n=0}
+            button.eq(n).trigger('click')
+        },1000);
+    };
+    $('#wrap').hover(
+        function(){
+            clearInterval(setIntervalId);
+        },
+        function(){
+            timer();
+        }
+    );
+    button.on({
+        click:function(){
+            var tg = $(this);
+            var i = tg.index();
+            if(current==tg.index()){
+                console.log('버튼이랑 이미지랑 current이 같구나!')
+                return;
+            }
+            button.removeClass('on');
+            tg.addClass('on');
+            move(i);
+        }
+    });
+    timer();
+    function move(i){ //i값을 전달받고.
+        var currentEl = visual.eq(current);
+        var nextEl = visual.eq(i);
+        currentEl.css({left:0}).stop().animate({left:'-100%'});
+        nextEl.css({left:'100%'}).stop().animate({left:'0%'});
+        current = i;
+    }
+
 });
