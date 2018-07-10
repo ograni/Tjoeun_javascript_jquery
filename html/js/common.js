56de2f28e5ad7f6c4b90ba964684a026053e35b2
더조은학원PC <ograni@hanmail.net>
2018-07-10 20:35:12 +0900

    7월 10일 1교시 백업

---
+++ html/js/common.js
@@ -0,0 +1,38 @@
+jQuery(document).ready(function(){
+
+	/* 로그인버튼 */
+	$('.login_wrap > a').on('click', function(){
+		$('#login_f').animate({top:'20px'},500);
+		return false;
+	});
+	$('.login_wrap .login_close_btn, input[alt="login"]').on('click',function(){
+		$('#login_f').animate({top:'-500px'},500);
+		return false;
+	});
+	/* top 이벤트배너 */
+
+
+	/* zoom 버튼 */
+
+
+	/* 인쇄버튼 */
+
+
+	/* 검색 창 안내 가이드 */
+
+
+	/* 전체메뉴 */
+
+
+	/* 전체 메뉴 닫기 버튼 */
+
+
+	/* 날짜표기 */
+
+
+	/* 관련사이트 이동 */
+
+
+	/* 퀵메뉴 */
+
+});
